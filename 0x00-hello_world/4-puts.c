6d5e4bbad0edd9b2b72793844595fd7f538cf154
Cynthia <islacynthia7@gmail.com>
2022-09-09 14:35:58 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,14 +8,13 @@
  *
  *    * Return: Displays a message to the terminal.
  *
- *     
+ *
  */
 
 int main(void)
 
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
 }
