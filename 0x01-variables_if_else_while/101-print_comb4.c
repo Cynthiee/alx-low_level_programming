d59e1bed3be80b87ce50adc8d48333056f71f25c
Cynthia <islacynthia7@gmail.com>
2022-09-10 15:45:03 -0700

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,6 +18,7 @@ int main(void)
 	for (n = 49; n < 58; n++)
 	{
 	for (o = (n + 50; 0 < 58; o++)
+	{
 	if (o > n && n > m)
 	{
 	putchar(m);
@@ -28,6 +29,7 @@ int main(void)
 	{
 	putchar(',');
 	putchar(' ');
+	}
 	}
 	}
 	}
