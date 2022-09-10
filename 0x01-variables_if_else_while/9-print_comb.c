4808e44c82be0726cbcc945b1bf4dedbbf1e6082
Cynthia <islacynthia7@gmail.com>
2022-09-10 14:09:44 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: this program prints all possible combinations
+ * of single-digit numbers.
+ *
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+
+	putchar('0' + num);
+	if (num < 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
