a09b137dd6345e672503993861814d5f900d93a5
Cynthia <islacynthia7@gmail.com>
2022-09-10 06:05:53 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - program execution starts
+ *
+ * Description: This program prints out all the letters of the
+ * alphabet in lowercase and uppercase plus a new line character.
+ *Return: 0 (Success)
+*/
+int main(void)
+{
+	char letter;
+
+	letter = 'a';
+
+	while (letter <= 'z')
+	{
+	putchar(letter);
+	letter++;
+	}
+	letter = 'A';
+
+	while (letter <= 'Z')
+	{
+	putchar(letter);
+	letter++;
+	}
+
+	putchar('\n)';
+	return (0);
+}
