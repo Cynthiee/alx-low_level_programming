370ef1d182db9e2d97667f7a24e6b275a58ac8b2
Cynthia <islacynthia7@gmail.com>
2022-10-05 16:29:29 -0700

    Argument

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program begins here.
+ * @argc: number of command line arguments
+ * @argv: array that contains the program command line argument.
+ * Return: 0 - success
+ */
+int main (int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is a file with codes on arguments in C.
