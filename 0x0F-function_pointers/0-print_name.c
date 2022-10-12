3f6d40b55768b1d8d624963f0014b30b9f260126
Cynthia <islacynthia7@gmail.com>
2022-10-12 15:18:09 -0700

    functions of p

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,15 @@
-#include <stdlib.h>
 #include "function_pointers.h"
-
+#include <stdlib.h>
 /**
  * print_name - prints a name
  * @name: The name to print
  * @f: pointer to the printing function
+ * Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
+	if (f == NULL)
 		return;
 	f(name);
 }
